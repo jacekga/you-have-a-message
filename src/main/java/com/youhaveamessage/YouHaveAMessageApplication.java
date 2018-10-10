06832e4cbcfb1b85cc3accc324d270fbba29c936
Jacek G <jgmarcuss@gmail.com>
2018-10-10 20:08:33 +0200

    Initial commit

---
+++ src/main/java/com/youhaveamessage/YouHaveAMessageApplication.java
@@ -0,0 +1,12 @@
+package com.youhaveamessage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouHaveAMessageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YouHaveAMessageApplication.class, args);
+	}
+}
